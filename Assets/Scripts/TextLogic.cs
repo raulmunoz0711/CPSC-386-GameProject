b62a720750027c5fff59a44babc768ef901eeab3
raulmunoz0711 <raulmunoz@csu.fullerton.edu>
2026-03-14 17:00:56 -0700

    Progress 2

---
+++ Assets/Scripts/TextLogic.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextLevel()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void returnToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void leveloOer()
+    {
+
+    }
+}
